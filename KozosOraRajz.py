65c2ce07cb44d33ea7c97b2da861bb50aa2b41cb
Husfield <marwmartw@gmail.com>
2026-05-18 20:27:10 +0200

    OraMutato

--- KozosOraRajz.py
+++ KozosOraRajz.py
@@ -11,12 +11,23 @@ turtle.goto(0,0)
 turtle.setheading(0)
 turtle.pendown()
 
-#2
+#perc
 
 turtle.color("blue")
 turtle.pensize(8)
 turtle.setheading(60)
 turtle.forward(140)
+turtle.penup()
+turtle.goto(0,0)
+turtle.setheading(0)
+turtle.pendown()
+
+#ora
+
+turtle.color("black")
+turtle.pensize(10)
+turtle.setheading(0)
+turtle.forward(60)
 turtle.penup()
 turtle.goto(0,0)
 turtle.setheading(0)
