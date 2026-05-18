9eae15c3925f08500134984a78cf2e0826decbef
KBalazs52 <kedvesbalazs2010@gmail.com>
2026-05-18 09:34:51 +0200

    masodperc mutato

---
+++ KozosOraRajz.py
@@ -0,0 +1,14 @@
+import turtle
+#alap beallitasok
+turtle.speed(0)
+turtle.pensize(5)
+turtle.color("red")
+#masodperc
+turtle.setheading(135)
+turtle.forward(75)
+turtle.penup()
+turtle.goto(0,0)
+turtle.setheading(0)
+turtle.pendown()
+
+turtle.done()
